ef1a6e41f6b52e904c92e554e608b8516271ce15
rockmanvnx6 <rockmanvnx6@gmail.com>
2022-03-03 23:51:39 +1100

    print node-v

--- index.ts
+++ index.ts
@@ -33,6 +33,7 @@ async function run(): Promise<void> {
 
         const workingDir = core.getInput("working-dir") || ".";
         const pkgManager = "npm";
+        await exec.exec(`node -v`);
         console.log(
             `Force installing your site's dependencies using ${pkgManager}.`
         );
